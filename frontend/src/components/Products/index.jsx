bf9d3e9bccf3154fe697a58a0ca22d068c2d0180
MVMAM <ediconpts9@gmail.com>
2023-12-06 19:58:49 +0500

    button

--- frontend/src/components/Products/index.jsx
+++ frontend/src/components/Products/index.jsx
@@ -1,18 +1,27 @@
-import './style.scss';
+import './style.scss'
 import { getFurnitures } from '../../conf/common'
 
-function Products(props) {
+function Products() {
 
     async function fetchFurnitures() {
-        let item = getFurnitures()
-        console.log(item)
+        let r = await getFurnitures()
+        console.log(r)
+    }
+    const btn_style = {
+        padding: "10px 30px",
+        width: '200px',
     }
 
     return (
         <div id="product-page">
             <h1>Products</h1>
+            <button style={btn_style} className='success-btn'
+                onClick={fetchFurnitures}
+            >
+                Get items
+            </button>
         </div>
-    )
+    );
 }
 
 export default Products;
