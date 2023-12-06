44499c12b4937cfca485353df42bae025e700260
MVMAM <ediconpts9@gmail.com>
2023-12-06 19:10:19 +0500

    auth.js

--- frontend/src/conf/common.js
+++ frontend/src/conf/common.js
@@ -1,4 +1,3 @@
-import { axiosCall } from './axios.js'
 import { toast } from 'react-toastify'
 const ACCESS_TOKEN_KEY = 'access_token';
 const REFRESH_TOKEN_KEY = 'refresh_token';
@@ -8,23 +7,8 @@ function isLoggedIn() {
     return token !== null;
 }
 
-function logoutEntirely() {
-    toast.success('Logged out successfully', { toastId: 999 })
-    localStorage.clear()
-    // try {
-    //     const data = {
-    //         "refresh": localStorage.getItem(REFRESH_TOKEN_KEY)
-    //     }
-    //     axiosCall("api/token/blacklist/", data, null, "POST")
-        
-    // } catch (e) {
-    //     console.error(e)
-    //     toast.error('Error during logout', { toastId: 999 })
-    // }
-}        
 
 export {
-    logoutEntirely,
     isLoggedIn,
     ACCESS_TOKEN_KEY,
     REFRESH_TOKEN_KEY
